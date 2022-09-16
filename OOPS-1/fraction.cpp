ed05b2327344cc6bc079b4a7124ebd60edeadff7
Shivansh-Gaur2 <96760872+Shivansh-Gaur2@users.noreply.github.com>
2022-09-16 23:05:09 +0530

    fraction class

---
+++ OOPS-1/fraction.cpp
@@ -0,0 +1,18 @@
+class Function{
+    private : 
+    int numerator;
+    int denominator;
+
+    public:
+
+    Fraction(int numerator, int denominator){
+        this -> numerator = numerator;
+        this -> denominator = denominator;
+    }
+
+    void print(){
+        cout << this -> numerator <<" / " << this -> denominator<<endl;
+    }
+
+    void add()
+}
