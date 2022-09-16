dd49731ab4d4c5f0c5a34ba1438e8e6f3d8f3b4d
Shivansh-Gaur2 <96760872+Shivansh-Gaur2@users.noreply.github.com>
2022-09-16 22:12:07 +0530

    constructor_destructor

--- OOPS-1/Inbuilt_constructors_destructors.cpp
+++ OOPS-1/Inbuilt_constructors_destructors.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+using namespace std;
+#include "constructors.cpp"
+// COPY CONSTRUCTOR
 
+int main(){
+    Student s1(10,1001);
+    cout <<"S1 : ";
+    s1.display();
+
+    Student s2(s1);
+    cout << "S2 : ";
+    s2.display();
+
+    Student *s3 = new Student(20,2001);
+    cout <<"S3 : ";
+    s3 -> display();
+
+    Student s4(*s3);
+    Student *s5 = new Student(*s3);
+
+    // COPY ASSIGNMENT OPERATOR(=)
+    s2 = s1;
+    *s3 = s1;
+    s2 = *s3;
+
+    delete s3;
+
+    // DESTRUCTOR 
+    // ~Student(){
+
+    // }
+}
