c63b7dc6c0931b1e12ba8296177ac56a254dc86d
Shivansh-Gaur2 <shivanshgaur.bt21ele@pec.edu.in>
2022-09-24 21:13:28 +0530

    better input

--- Linked list/input1.cpp
+++ Linked list/input1.cpp
@@ -12,21 +12,20 @@ class Node {
 	}
 };
 
-Node* takeInput() {
+Node* takeInput_better() {
 	int data;
 	cin >> data;
 	Node *head = NULL;
+    Node *tail = NULL;
 	while(data != -1) {
 		Node *newNode = new Node(data);
 		if(head == NULL) {
 			head = newNode;
+            tail = newNode;
 		}
 		else {
-			Node *temp = head;
-			while(temp -> next != NULL) {
-				temp = temp -> next;
-			}
-			temp -> next = newNode;
+            tail -> next = newNode;
+            tail = tail -> next;
 		}
 
 		cin >> data;
@@ -43,7 +42,7 @@ void print(Node *head) {
 
 int main() {
 
-	Node *head = takeInput();
+	Node *head = takeInput_better();
 	print(head);
 	
 	/*
